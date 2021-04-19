b7051c63fa54771e11cc726f69633b4825ca9982
Muhammad Buhari <gmbdairy@gmail.com>
2021-04-19 04:05:33 +0100

    restructuring with redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
